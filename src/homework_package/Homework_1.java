4e0bddc5a99acef1d116e450cdd71e700963a79e
RedPh <lmb7gq@umsystem.edu>
2024-02-01 21:03:39 -0600

    Package
    This pacakge is where you write name of our team and each of our git hub
    id's I left an example blank print to show where next team member needs
    to put their name and git hub ID

---
+++ src/homework_package/Homework_1.java
@@ -0,0 +1,11 @@
+package homework_package;
+
+public class Homework_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Team Member 1: Logan Bird - Git hubID : LMB7GQ");
+		System.out.println("Team Member 2 : [NAME] - Git hubID [NAME]");
+	}
+
+}
