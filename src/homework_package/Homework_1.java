9e0deaa3b6d686ea0c80891501d4258e43037ce5
dionb <dionb@Mogira>
2024-02-02 10:28:08 -0600

    added name and ID

--- src/homework_package/Homework_1.java
+++ src/homework_package/Homework_1.java
@@ -6,6 +6,7 @@ public class Homework_1 {
 		// TODO Auto-generated method stub
 		System.out.println("Team Member 1: Logan Bird - Git hubID : LMB7GQ");
 		System.out.println("Team Member 2 : Ryan Esparza - Git hubID Av0cad0T0ast");
+		System.out.println("Team Member 3: Dion Burns - Git hubID: dionaburns1954");
 	}
 
 }
