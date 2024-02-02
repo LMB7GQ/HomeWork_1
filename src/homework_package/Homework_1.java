8495d4c715eddb3aedc08a877d32f6e7dac7a630
azfcf <azfcf@umsystem.edu>
2024-02-02 16:00:38 -0600

    add team member

--- src/homework_package/Homework_1.java
+++ src/homework_package/Homework_1.java
@@ -5,8 +5,9 @@ public class Homework_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Team Member 1: Logan Bird - Git hubID : LMB7GQ");
-		System.out.println("Team Member 2 : Ryan Esparza - Git hubID Av0cad0T0ast");
+		System.out.println("Team Member 2: Ryan Esparza - Git hubID Av0cad0T0ast");
 		System.out.println("Team Member 3: Dion Burns - Git hubID: dionaburns1954");
+		System.out.println("Team Member 4: Albert Zhou - Git hubID: azfcf");
 	}
 
 }
