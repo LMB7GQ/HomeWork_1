40bfe809c3703d392176a2037268814f6f9d686b
Ryan Esparza <Ryan Esparza@DESKTOP-MVS95Q2>
2024-02-01 23:47:59 -0600

    Added my name and ID

--- src/homework_package/Homework_1.java
+++ src/homework_package/Homework_1.java
@@ -5,7 +5,7 @@ public class Homework_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Team Member 1: Logan Bird - Git hubID : LMB7GQ");
-		System.out.println("Team Member 2 : [NAME] - Git hubID [NAME]");
+		System.out.println("Team Member 2 : Ryan Esparza - Git hubID Av0cad0T0ast");
 	}
 
 }
